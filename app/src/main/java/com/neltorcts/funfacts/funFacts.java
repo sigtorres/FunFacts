eb55ef15cd34160ad4de18ad86b602db82692010
Nelson Torres <nelson@neltorcts.com>
2015-11-09 23:43:25 -0600

    added extra facts for the button.

--- app/src/main/java/com/neltorcts/funfacts/funFacts.java
+++ app/src/main/java/com/neltorcts/funfacts/funFacts.java
@@ -8,6 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class funFacts extends AppCompatActivity {
 
@@ -15,6 +19,30 @@ public class funFacts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        //declare our view variables and assign them the views from layout file
+        final TextView factLabelZ = (TextView) findViewById(R.id.factTextView);
+        Button showFactButtonZ = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                //the button was clicked so update the fact label with new fact
+                //String fact = "Ostriches can run faster than horses!";
+                String fact = "";
+                //randomly select a fact
+                Random randomGenerator = new Random(); //construct a new random number generator
+                int randomNumber = randomGenerator.nextInt(3);
+                fact = randomNumber + "";
+
+                //update the label with our dynamic fact
+                factLabelZ.setText(fact);
+
+            };
+        };
+        showFactButtonZ.setOnClickListener(listener);
+
+
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
